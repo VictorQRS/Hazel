42538ca61a7dc32c56b7df475146d4226576620b
torq <quezado_8588@hotmail.com>
2018-11-15 12:46:37 -0200

    Added spdlog and basic logging API/macros

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,10 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized logger");
+	HZ_INFO("Hello, {0}", "World");
+
 	auto app = Hazel::CreateApplication();
 	app->run();
 	delete app;
