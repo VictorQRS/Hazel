fa9eb79a3c59e975dfd6e6d841b39bdc374866a3
torq <quezado_8588@hotmail.com>
2019-02-20 00:49:35 -0300

    Added input polling

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -6,6 +6,9 @@
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
+
+#include "Hazel/Input.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 // ---Entry Point ---
--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Hazel {
 
 	Application* Application::s_Instance = nullptr;
